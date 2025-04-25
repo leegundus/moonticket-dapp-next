ac2b965839410cd50c6593930a888bc1cc5e142a
leegundus <leegundus@gmail.com>
2025-04-25 17:56:41 -0500

    wallet fix

--- wallet/WalletProvider.js
+++ wallet/WalletProvider.js
@@ -3,23 +3,20 @@ import {
   WalletProvider,
 } from "@solana/wallet-adapter-react";
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 const endpoint = "https://mainnet.helius-rpc.com/?api-key=47d9c64e-1d5a-4623-abaf-ee532aca9eaa";
 
-const WalletWrapper = ({ children }) => {
-  const isPhantomInstalled =
-    typeof window !== "undefined" && window.solana?.isPhantom;
-
-  const wallets = isPhantomInstalled ? [new PhantomWalletAdapter()] : [];
-
+const dynamicWalletProvider = (component) => {
   return (
     <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets} autoConnect>
-        {children}
+      <WalletProvider wallets={[new PhantomWalletAdapter()]} autoConnect>
+          {component}
       </WalletProvider>
     </ConnectionProvider>
   );
 };
 
-export default WalletProvider;
+export default dynamicWalletProvider;
