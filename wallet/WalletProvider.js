2fdde8e9f7e59b093035c7adbe290f1c04ec88eb
leegundus <leegundus@gmail.com>
2025-04-25 17:22:06 -0500

    // fix

--- wallet/WalletProvider.js
+++ wallet/WalletProvider.js
@@ -13,9 +13,7 @@ const dynamicWalletProvider = (component) => {
   return (
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={[new PhantomWalletAdapter()]} autoConnect>
-       // <WalletModalProvider>
           {component}
-       // </WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
   );
