cfaa49375ba8d3bb486832f759cd1a6f68ef6212
leegundus <leegundus@gmail.com>
2025-06-14 22:12:59 -0500

    Remove phantom install popup when already installed

--- wallet/WalletProvider.js
+++ wallet/WalletProvider.js
@@ -4,25 +4,21 @@ import {
 } from "@solana/wallet-adapter-react";
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import dynamic from "next/dynamic";
 
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 const endpoint = process.env.NEXT_PUBLIC_RPC_URL;
 
+// Always define the wallet
+const wallets = [new PhantomWalletAdapter()];
+
 const dynamicWalletProvider = (component) => {
-  // Check for Phantom only on the client side
-  const isPhantomAvailable =
-    typeof window !== "undefined" &&
-    window?.solana &&
-    window.solana.isPhantom;
-
-  const wallets = isPhantomAvailable ? [new PhantomWalletAdapter()] : [];
-
   return (
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>{component}</WalletModalProvider>
+        <WalletModalProvider>
+          {component}
+        </WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
   );
