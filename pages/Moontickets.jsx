52d6c4e745bffa22fcb16a7a5c68b633f1d0674a
leegundus <leegundus@gmail.com>
2025-09-10 08:47:38 -0500

    moonticket page update

--- pages/Moontickets.jsx
+++ pages/Moontickets.jsx
@@ -2,15 +2,18 @@ import dynamic from "next/dynamic";
 const Moontickets = dynamic(()=>import("../components/Moontickets"), { ssr:false });
 
 export default function Page() {
-  <div className="min-h-screen bg-black text-yellow-400 flex flex-col items-center justify-start py-10 space-y-8 pt-40">
-      {/* Logo */}
-      <img
-        src="/moontickets-banner.png"
-        alt="Moonticket Banner"
-        className="w-72 md:w-[28rem] lg:w-[36rem] mx-auto"
-      />
+  return (
+    <div className="min-h-screen bg-black text-yellow-400>
+      <div className="mx-auto max-w-5x1 px-4 pt-40 pb-10">
+        {/* Logo */}
+        <img
+          src="/moontickets-banner.png"
+          alt="Moonticket Banner"
+          className="w-72 md:w-[28rem] lg:w-[36rem] mx-auto"
+        />
 
-  // Pull in your wallet + price hooks here (publicKey, tixPriceUSD, tixBalance)
-  // and pass as props to <Moontickets />
-  return <Moontickets /* publicKey={...} tixPriceUSD={...} tixBalance={...} onRefresh={...} */ />;
+        <Moontickets />
+      </div>
+    </div>
+   );
 }
