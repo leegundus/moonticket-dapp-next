138d873a9ed077d24646fd798224585406eba5d4
leegundus <leegundus@gmail.com>
2025-09-17 12:42:56 -0500

    add past tickets

--- components/Moontickets.jsx
+++ components/Moontickets.jsx
@@ -224,7 +224,7 @@ export default function Moontickets({ publicKey, tixBalance, onRefresh }) {
   function TicketImages({ t }) {
     const nums = [t.num1, t.num2, t.num3, t.num4].sort((a,b)=>a-b);
     const wrapper = { display:"inline-flex", gap:6, alignItems:"center", verticalAlign:"middle" };
-    const imgStyle = { width:256, height:256, objectFit:"contain" };
+    const imgStyle = { width:128, height:128, objectFit:"contain" };
     return (
       <span style={wrapper}>
         {nums.map((n,i)=>(
