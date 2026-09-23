5611b3db0a66a34b925dae2ea768037a4fb1cff2
leegundus <140096966+leegundus@users.noreply.github.com>
2026-09-23 09:36:10 -0500

    Fix Mashers ERC721 constructor import

--- mashers-chain/contracts/Mashers.sol
+++ mashers-chain/contracts/Mashers.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import {ERC2981} from "@openzeppelin/contracts/token/common/ERC2981.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
